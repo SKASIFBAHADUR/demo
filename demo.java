4dae3fc1c131851e692836ca4c41757edebcfe6d
SHAIK BAHADUR <skbahadur19@gmail.com>
2025-11-27 11:41:22 +0530

    added name and print function

--- demo.java
+++ demo.java
@@ -1,5 +1,8 @@
 public class demo {
     public static void main(String[] args) {
         int fig=5;
+        System.out.println("printing fig ");
+        String name="bahadur";
+        System.out.println(name);
     }
 }
